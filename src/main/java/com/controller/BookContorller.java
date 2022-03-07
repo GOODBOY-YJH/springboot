1451a5948c0cc690c2427de1070d9e6ffb1d9751
GOODBOY-YJH <1613777456@qq.com>
2022-03-07 16:31:06 +0800

    添加一个测试文件

---
+++ src/main/java/com/controller/BookContorller.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Rest模式
+@RestController
+@RequestMapping("/books")
+public class BookContorller {
+
+    @GetMapping
+    public String getById(){
+        System.out.println("springBoot is running....");
+        return "springBoot is running....";
+    }
+
+}
