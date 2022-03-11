aae8f270e9273f4c2ec5f2ff4e5727b1a8ec4c52
GOODBOY-YJH <1613777456@qq.com>
2022-03-11 17:07:57 +0800

    整合案例表现层标准开发

---
+++ src/main/java/com/example/controller/AccountController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+
+import com.example.domain.Account;
+import com.example.service.IAccountService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+    @Autowired
+    private IAccountService accountService;
+
+    @GetMapping
+    public List<Account> getAll(){
+        return accountService.list();
+    }
+
+    @PostMapping
+    public Boolean save(@RequestBody Account account){
+        return accountService.save(account);
+    }
+
+    @DeleteMapping("{id}")
+    public Boolean delete(@PathVariable Integer id){
+        return accountService.removeById(id);
+    }
+    @GetMapping("{id}")
+    public Account getById(@PathVariable Integer id){
+        return accountService.getById(id);
+    }
+
+}
