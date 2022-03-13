4b9d6390dddd9bddda2a5d93ff1fa4eaccb4f83b
GOODBOY-YJH <1613777456@qq.com>
2022-03-13 15:39:07 +0800

    为当前测试类导入测试类专用配置

--- src/test/java/com/example/DatasourceTestCase.java
+++ src/test/java/com/example/DatasourceTestCase.java
@@ -1,8 +1,7 @@
 package com.example;
 
-import com.example.config.MPConfig;
+import com.example.config.MsgConfig;
 import com.example.domain.MyDateSource;
-import com.example.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,15 +10,19 @@ import org.springframework.context.annotation.Import;
 // properties args 属性可以给测试添加临时的属性配置
 
 // @SpringBootTest(properties = {"键=值"})
-@SpringBootTest(args = {"--键=值"})
-@Import(MPConfig.class) // 加载额外配置类
+@SpringBootTest//(args = {"--键=值"})
+@Import(MsgConfig.class) // 加载额外配置类
 public class DatasourceTestCase {
 
     @Autowired
     private MyDateSource myDateSource;
 
+    @Autowired
+    private String msg;
+
     @Test
     void testGetById() {
         System.out.println(myDateSource.getUrl());
+        System.out.println(msg);
     }
 }
---
+++ src/test/java/com/example/config/MsgConfig.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MsgConfig {
+
+    @Bean
+    public String msg(){
+        return "string jiaZai";
+    }
+}
